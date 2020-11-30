0128feb4042d4e46249e77022adeca5ea94c6b08
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-30 15:44:26 +0100

    Header - Navbar completed

--- action.js
+++ action.js
@@ -8,7 +8,10 @@ creando una struttura dati che consenta di inserire i link nelle navbar.
 let app = new Vue({
   el: '#root',
   data: {
-
+    nav_logo: {
+      url: 'avadabarbers-logo-x1',
+      alt: 'Avada Barbers logo',
+    },
   },
   methods: {
 
