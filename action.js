58fc395305859df060dc59791d114b58c843f16e
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-30 23:02:00 +0100

    Footer top & credits completed in HTML & VueJS (style CSS to be finished)

--- action.js
+++ action.js
@@ -1,25 +1,61 @@
-/* ASSIGNMENT
-Aggiungere Header e Footer con VueJs
-creando una struttura dati che consenta di inserire i link nelle navbar.
-*/
-
 // ------------------------------ VUE JS ------------------------------
 
 let app = new Vue({
   el: '#root',
   data: {
-    nav_logo: {
-      url: 'avadabarbers-logo-x1',
+    logo: {
+      url: 'avadabarbers_logo',
       alt: 'Avada Barbers logo',
     },
     page_title: 'Barber Shop',
     jumbo_slogan: 'The Pinnacle of Male Grooming',
     jumbo_img: {
-      url: 'avadabarbers_hero_focalmirror-400x550',
-      alt: 'Barber working',
+      url: 'jumbotron_poster',
+      alt: 'Barber hairstyling',
     },
-  },
+    footer_products_img: {
+      url: 'footer_products',
+      alt: 'Shaving products',
+    },
+    footer_contacts: {
+      holder: 'Avada Barbers',
+      address: {
+        street: '123 New York Street',
+        city: 'New York City',
+      },
+      email: 'info@yourwebsite.com',
+      tel: '+1(555)555-1212',
+    },
+    footer_social_media: [
+      {
+        name: 'facebook-f',
+        url: '#',
+      },
+      {
+        name: 'twitter',
+        url: '#',
+      },
+      {
+        name: 'youtube',
+        url: '#',
+      },
+      {
+        name: 'instagram',
+        url: '#',
+      },
+    ],
+    footer_barber_img: {
+      url: 'footer_poster',
+      alt: 'Barber hairstyling',
+    },
+    footer_credits: [
+      '\u00A9 Copyright 2012-2020', // \u00A9 JS unicode for © Copyright
+      'Avada Theme by ThemeFusion',
+      'All Rights Reserved',
+      'Powered by WordsPress',
+    ],
+  },  // Closing data
   methods: {
 
-  },
+  },  // Closing methods
 });
