da5459fe4292a6b36e19cb4a6666f330c8886e9d
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-30 14:10:57 +0100

    Scaffolding completed

---
+++ action.js
@@ -0,0 +1,16 @@
+/* ASSIGNMENT
+Aggiungere Header e Footer con VueJs
+creando una struttura dati che consenta di inserire i link nelle navbar.
+*/
+
+// ------------------------------ VUE JS ------------------------------
+
+let app = new Vue({
+  el: '#root',
+  data: {
+
+  },
+  methods: {
+
+  },
+});
